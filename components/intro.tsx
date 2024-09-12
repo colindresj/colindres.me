03fd6849f8357424370ad99928ffcce22b421cb5
JC <jorge.e.colindres@gmail.com>
2024-09-12 19:05:01 -0400

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -10,18 +10,18 @@ const Intro = () => {
       <h1 className="text-xl">Jorge Colindres</h1>
       <div className="mt-2">
         <p className="mb-4">
-          I'm currently a Partner at{' '}
-          <a
+          I'm currently co-founder of <a
+            href="https://radical-ai.com"
+            className="border-b-2 border-day-accent text-day-accent hover:bg-day-accent hover:text-day-bg dark:border-night-accent dark:text-night-accent dark:hover:bg-night-accent dark:hover:text-night-bg"
+          >
+            Radical AI
+          </a>. Before that, I was a Partner at <a
             href="https://alleycorp.com"
             className="border-b-2 border-day-accent text-day-accent hover:bg-day-accent hover:text-day-bg dark:border-night-accent dark:text-night-accent dark:hover:bg-night-accent dark:hover:text-night-bg"
           >
             AlleyCorp
           </a>
-          , an NYC-based venture studio and investment firm. I spend a lot of
-          my time working with founders and thinking about new startups. Before
-          AlleyCorp, I worked as a software engineer at a few venture-backed
-          early-stage companies. I was on the founding team of Workframe
-          (acquired NYSE:NMRK), an AlleyCorp incubation.
+          , an NYC-based venture studio and investment firm, and part of the founding team of two other startups.
         </p>
         <p>
           I received a BS and a BA from Boston University, as well as an MBA
